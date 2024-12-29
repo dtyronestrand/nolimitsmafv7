7749fc9482cbf378a93fcf27920fcbe41ce00f26
dtyronestrand <154398+dtyronestrand@noreply.github.com>
2024-12-29 14:59:17 +0000

    ?

--- src/routes/[[preview=preview]]/[uid]/+page.server.ts
+++ src/routes/[[preview=preview]]/[uid]/+page.server.ts
@@ -1,21 +1,24 @@
-import { asText, mapSliceZone } from '@prismicio/client'
-import { mappers } from '$lib/slices/mappers'
-import { createClient } from '$lib/prismicio'
+import { asText, mapSliceZone } from '@prismicio/client';
+
+import { createClient } from '$lib/prismicio';
+import { mappers } from '$lib/slices/mappers';
+
 
 export async function load({ params, fetch, cookies }) {
-  const client = createClient({ fetch, cookies })
+	const client = createClient({ fetch, cookies });
 
-  const page = await client.getByUID('page', params.uid)
-  const slices = await mapSliceZone(page.data.slices, mappers, { client })
-  return {
-    slices,
-    title: asText(page.data.title),
-    meta_description: page.data.meta_description,
-    meta_title: page.data.meta_title,
-    meta_image: page.data.meta_image.url,
-  }
+	const page = await client.getByUID('page', params.uid);
+	const slices = await mapSliceZone(page.data.slices, mappers, { client });
+
+	return {
+		slices,
+		title: asText(page.data.title),
+		meta_description: page.data.meta_description,
+		meta_title: page.data.meta_title,
+		meta_image: page.data.meta_image.url
+	};
 }
 
 export function entries() {
-  return [{ uid: 'page' }]
+	return [{ uid:'page' }];
 }
