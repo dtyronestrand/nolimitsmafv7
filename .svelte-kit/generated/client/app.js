be8e5ea914798c9d686b5e5536d0b5f10541f815
dtyronestrand <1354732+dtyronestrand@noreply.githubuser.com>
2025-01-06 21:34:31 -0500

    fuck git fe#

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -1,3 +1,6 @@
+import * as client_hooks from '../../../src/hooks.client.ts';
+
+
 export { matchers } from './matchers.js';
 
 export const nodes = [
@@ -17,21 +20,23 @@ export const nodes = [
 	() => import('./nodes/13'),
 	() => import('./nodes/14'),
 	() => import('./nodes/15'),
-	() => import('./nodes/16')
+	() => import('./nodes/16'),
+	() => import('./nodes/17')
 ];
 
 export const server_loads = [0,3];
 
 export const dictionary = {
 		"/[[preview=preview]]/(users)/location/[uid]": [~5,[2]],
-		"/logout": [12],
-		"/profile": [13],
+		"/login": [12],
+		"/logout": [13],
+		"/profile": [14],
 		"/[[preview=preview]]/(users)/program/[uid]": [~6,[2,3]],
 		"/[[preview=preview]]/(users)/program/[uid]/testingapp": [~7,[2,3]],
 		"/[[preview=preview]]/(users)/program/[uid]/video/[uid]": [~8,[2,3]],
-		"/register": [14],
-		"/slice-simulator": [15],
-		"/unauthorized": [16],
+		"/register": [15],
+		"/slice-simulator": [16],
+		"/unauthorized": [17],
 		"/[[preview=preview]]/(users)/videoindex": [~10,[2]],
 		"/[[preview=preview]]/(users)/video/[uid]": [~9,[2]],
 		"/[[preview=preview]]": [~4],
@@ -39,8 +44,8 @@ export const dictionary = {
 	};
 
 export const hooks = {
-	handleError: (({ error }) => { console.error(error) }),
-	
+	handleError: client_hooks.handleError || (({ error }) => { console.error(error) }),
+	init: client_hooks.init,
 	reroute: (() => {}),
 	transport: {}
 };
